033a3fca68b737179b50bb5c0ad33b390e37a9ff
Lucas F. Simoni <lucasfrsi@gmail.com>
2020-09-24 16:24:48 -0300

    Update post component

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -9,8 +9,39 @@ import svg from '../../assets/svg/sprite.svg';
 
 import styles from './style.scss';
 
-const Post = () => {
+const Post = ({
+  _id,
+  text,
+  likes,
+  comments,
+  createdAt,
+  updatedAt,
+  user,
+  nickname,
+  avatar,
+  loggedUserId,
+}) => {
   const [loading, setLoading] = useState(true);
+  const [postLikes, setPostLikes] = useState(0);
+  const [postDislikes, setPostDislikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(null);
+
+  useEffect(() => {
+    let likesCount = 0;
+    let dislikesCount = 0;
+
+    likes.forEach((item) => {
+      if (item.type === 'like') likesCount += 1;
+      if (item.type === 'dislike') dislikesCount += 1;
+      if (item.user === loggedUserId) {
+        if (item.type === 'like') setIsLiked(true);
+        if (item.type === 'dislike') setIsLiked(false);
+      }
+    });
+
+    setPostLikes(likesCount);
+    setPostDislikes(dislikesCount);
+  }, [likes, loggedUserId]);
 
   const handleOnLoad = () => {
     setLoading(false);
@@ -22,13 +53,28 @@ const Post = () => {
     img.src = defaultAvatar;
   };
 
+  const countComments = () => {
+    let count;
+    let comment = ' Comments';
+
+    if (comments.length === 0) {
+      count = 'No';
+    } else if (comments.length === 1) {
+      count = 1;
+      comment = ' Comment';
+    } else {
+      count = comments.length;
+    }
+
+    return <p className={styles.postDiscussionText}><span>{count}</span>{comment}</p>;
+  };
+
   return (
     <div className={styles.post}>
-
       <div className={styles.postUser}>
         {loading ? <LoadingSpinner size={styles.loadingSpinner} /> : null}
         <img
-          src={/* posts.posts[0].user.avatar || */ defaultAvatar}
+          src={avatar || defaultAvatar}
           alt="User"
           className={styles.userPhoto}
           onLoad={handleOnLoad}
@@ -37,7 +83,7 @@ const Post = () => {
             display: loading ? 'none' : 'inline-block',
           }}
         />
-        <h4>xCloudFFVII</h4>
+        <h4>{nickname}</h4>
       </div>
 
       <div className={styles.postContent}>
@@ -45,61 +91,60 @@ const Post = () => {
         <div className={styles.postData}>
 
           <p className={styles.postText}>
-            Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
-            there live the blind texts. Separated they live in Bookmarksgrove right at the coast of
-            the Semantics, a large language ocean. A small river named Duden flows by their place
-            and supplies it with the necessary.
-            Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
-            there live the blind texts. Separated they live in Bookmarksgrove right at the coast of
-            the Semantics, a large language ocean. A small river named Duden flows by their place
-            and supplies it with the necessary.
+            {text}
           </p>
-
-          <div className={styles.postDate}>
-
-            <p className={styles.postCreated}>
-              Aug 25th at 10:13pm
-            </p>
-
-            <p className={styles.postEdited}>
-              Edited
-            </p>
-
-          </div>
 
         </div>
 
-        <div className={styles.postInteractions}>
+        <div>
+          <div className={styles.postDate}>
 
-          <div className={styles.postLikeComment}>
-            <div className={styles.postLikeBox}>
-              <div className={styles.postLike}>
-                <svg>
-                  <use xlinkHref={`${svg}#icon-thumbs-up`} />
-                </svg>
-                <span>17</span>
-              </div>
-              <div className={styles.postDislike}>
-                <svg>
-                  <use xlinkHref={`${svg}#icon-thumbs-down`} />
-                </svg>
-                <span>2</span>
-              </div>
-            </div>
-            <div className={styles.postDiscussion}>
-              <p className={styles.postDiscussionText}><span>34</span> Comments</p>
-            </div>
+            <p className={styles.postCreated}>
+              {/* Aug 25th at 10:13pm */}
+              {createdAt}
+            </p>
+
+            <p className={styles.postEdited}>
+              {/* Edited */}
+              {updatedAt !== createdAt ? 'Edited' : null} {/* WRONG, CHANGE LOGIC */}
+            </p>
+
           </div>
 
-          <div className={styles.postEditDelete}>
-            <svg>
-              <use xlinkHref={`${svg}#icon-edit`} />
-            </svg>
-            <svg>
-              <use xlinkHref={`${svg}#icon-trash`} />
-            </svg>
-          </div>
+          <div className={styles.postInteractions}>
 
+            <div className={styles.postLikeComment}>
+              <div className={styles.postLikeBox}>
+                <div className={`${styles.postLike} ${isLiked === true ? styles.postLikeActive : null}`}>
+                  <svg>
+                    <use xlinkHref={`${svg}#icon-thumbs-up`} />
+                  </svg>
+                  <span>{postLikes}</span>
+                </div>
+                <div className={`${styles.postDislike} ${isLiked === false ? styles.postDislikeActive : null}`}>
+                  <svg>
+                    <use xlinkHref={`${svg}#icon-thumbs-down`} />
+                  </svg>
+                  <span>{postDislikes}</span>
+                </div>
+              </div>
+              <div className={styles.postDiscussion}>
+                {countComments()}
+              </div>
+            </div>
+
+            {user === loggedUserId ? (
+              <div className={styles.postEditDelete}>
+                <svg>
+                  <use xlinkHref={`${svg}#icon-edit`} />
+                </svg>
+                <svg>
+                  <use xlinkHref={`${svg}#icon-trash`} />
+                </svg>
+              </div>
+            ) : null}
+
+          </div>
         </div>
 
       </div>
@@ -109,11 +154,20 @@ const Post = () => {
 };
 
 Post.propTypes = {
-
+  _id: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  likes: PropTypes.arrayOf(PropTypes.object).isRequired,
+  comments: PropTypes.arrayOf(PropTypes.object).isRequired,
+  createdAt: PropTypes.string.isRequired,
+  updatedAt: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired,
+  nickname: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  loggedUserId: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  userId: state.auth.user._id,
+  loggedUserId: state.auth.user._id,
 });
 
 export default connect(mapStateToProps)(Post);
--- src/components/Profile/Overview/index.jsx
+++ src/components/Profile/Overview/index.jsx
@@ -9,111 +9,146 @@ import svg from '../../../assets/svg/sprite.svg';
 import Post from '../../Post';
 import Moment from '../../Moment';
 
-const Overview = ({ posts, moments }) => (
-  <div className={styles.overview}>
+const Overview = ({ posts, moments, user }) => {
+  const renderLastPost = () => {
+    // const lastPostIndex = posts.length - 1;
+    // const lastPost = posts[lastPostIndex];
+    const lastPost = posts[3];
+    console.log(lastPost);
+    return (
+      <Post
+        className={styles.recentActivityPost}
+        _id={lastPost._id}
+        text={lastPost.text}
+        likes={lastPost.likes}
+        comments={lastPost.comments}
+        createdAt={lastPost.createdAt}
+        updatedAt={lastPost.updatedAt}
+        user={lastPost.user}
+        nickname={user.nickname}
+        avatar={user.avatar}
+      />
+    );
+  };
 
-    <div className={styles.container}>
-      <div className={styles.gamerInfo}>
-        <div className={styles.heading}>
-          <h3>Gamer Info</h3>
+  const renderLastMoment = () => {
+    const lastMomentIndex = moments.length - 1;
+    const lastMoment = moments[lastMomentIndex];
+    return (
+      <Moment
+        _id={lastMoment._id}
+        title={lastMoment.title}
+        text={lastMoment.text}
+        imageUrl={lastMoment.imageUrl}
+        reactions={lastMoment.reactions}
+        createdAt={lastMoment.createdAt}
+        user={lastMoment.user}
+        nickname={user.nickname}
+        avatar={user.avatar}
+      />
+    );
+  };
+
+  return (
+    <div className={styles.overview}>
+
+      <div className={styles.container}>
+        <div className={styles.gamerInfo}>
+          <div className={styles.heading}>
+            <h3>Gamer Info</h3>
+          </div>
+          <div className={styles.gamerData}>
+            <h4 className={styles.gamerDataHeading}>BIO</h4>
+            <p>
+              But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure r
+            </p>
+            <br />
+            <p>Favorite Game: <span>League of Legends</span></p>
+            <p>Recently Played: <span>Fortnite</span></p>
+          </div>
         </div>
-        <div className={styles.gamerData}>
-          <h4 className={styles.gamerDataHeading}>BIO</h4>
-          <p>
-            But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure r
-          </p>
-          <br />
-          <p>Favorite Game: <span>League of Legends</span></p>
-          <p>Recently Played: <span>Fortnite</span></p>
+        <div className={styles.gaming}>
+          <div className={styles.heading}>
+            <h3>Platforms</h3>
+          </div>
+          <div className={styles.gamingData}>
+            {/* <p className={styles.gamingDataTitle}>This gamer is part of the following platforms: </p> */}
+            <div className={styles.platform}>
+              <svg className={styles.icon}>
+                <use xlinkHref={`${svg}#icon-nintendoswitch`} />
+              </svg>
+              <p className={styles.gamingDataInfo}><span>Nintendo</span> as <span>Cloud</span></p>
+            </div>
+            <div className={styles.platform}>
+              <svg className={styles.icon}>
+                <use xlinkHref={`${svg}#icon-playstation`} />
+              </svg>
+              <p className={styles.gamingDataInfo}><span>Playstation Network</span> as <span>xCloudyTales</span></p>
+            </div>
+            <div className={styles.platform}>
+              <svg className={styles.icon}>
+                <use xlinkHref={`${svg}#icon-xbox`} />
+              </svg>
+              <p className={styles.gamingDataInfo}><span>Xbox Live</span> as <span>xCloud#4321</span></p>
+            </div>
+            <div className={styles.platform}>
+              <svg className={styles.icon}>
+                <use xlinkHref={`${svg}#icon-epicgames`} />
+              </svg>
+              <p className={styles.gamingDataInfo}><span>Epic Games</span> as <span>Cloud</span></p>
+            </div>
+            <div className={styles.platform}>
+              <svg className={styles.icon}>
+                <use xlinkHref={`${svg}#icon-steam`} />
+              </svg>
+              <p className={styles.gamingDataInfo}><span>Steam</span> as <span>Cloud</span></p>
+            </div>
+            <div className={styles.platform}>
+              <svg className={styles.icon}>
+                <use xlinkHref={`${svg}#icon-discord`} />
+              </svg>
+              <p className={styles.gamingDataInfo}><span>Discord</span> as <span>xCloudFF#1234</span></p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.twitch}>
+          <div className={styles.heading}>
+            <h3>Twitch Channel</h3>
+          </div>
+          <iframe
+            title="twitch"
+            src={`https://player.twitch.tv/?channel=swimstrim&parent=${window.location.hostname}&muted=true`}
+            height="300"
+            width="400"
+            frameBorder="1"
+            scrolling="no"
+            allowFullScreen
+          />
         </div>
       </div>
-      <div className={styles.gaming}>
-        <div className={styles.heading}>
-          <h3>Platforms</h3>
-        </div>
-        <div className={styles.gamingData}>
-          {/* <p className={styles.gamingDataTitle}>This gamer is part of the following platforms: </p> */}
-          <div className={styles.platform}>
-            <svg className={styles.icon}>
-              <use xlinkHref={`${svg}#icon-nintendoswitch`} />
-            </svg>
-            <p className={styles.gamingDataInfo}><span>Nintendo</span> as <span>Cloud</span></p>
-          </div>
-          <div className={styles.platform}>
-            <svg className={styles.icon}>
-              <use xlinkHref={`${svg}#icon-playstation`} />
-            </svg>
-            <p className={styles.gamingDataInfo}><span>Playstation Network</span> as <span>xCloudyTales</span></p>
-          </div>
-          <div className={styles.platform}>
-            <svg className={styles.icon}>
-              <use xlinkHref={`${svg}#icon-xbox`} />
-            </svg>
-            <p className={styles.gamingDataInfo}><span>Xbox Live</span> as <span>xCloud#4321</span></p>
-          </div>
-          <div className={styles.platform}>
-            <svg className={styles.icon}>
-              <use xlinkHref={`${svg}#icon-epicgames`} />
-            </svg>
-            <p className={styles.gamingDataInfo}><span>Epic Games</span> as <span>Cloud</span></p>
-          </div>
-          <div className={styles.platform}>
-            <svg className={styles.icon}>
-              <use xlinkHref={`${svg}#icon-steam`} />
-            </svg>
-            <p className={styles.gamingDataInfo}><span>Steam</span> as <span>Cloud</span></p>
-          </div>
-          <div className={styles.platform}>
-            <svg className={styles.icon}>
-              <use xlinkHref={`${svg}#icon-discord`} />
-            </svg>
-            <p className={styles.gamingDataInfo}><span>Discord</span> as <span>xCloudFF#1234</span></p>
-          </div>
-        </div>
-      </div>
-      <div className={styles.twitch}>
-        <div className={styles.heading}>
-          <h3>Twitch Channel</h3>
-        </div>
-        <iframe
-          title="twitch"
-          src={`https://player.twitch.tv/?channel=swimstrim&parent=${window.location.hostname}&muted=true`}
-          height="300"
-          width="400"
-          frameBorder="1"
-          scrolling="no"
-          allowFullScreen
-        />
-      </div>
-    </div>
 
-    <div className={`${styles.heading} ${styles.endHeading}`}>
-      <h3>Recent Activity</h3>
-    </div>
-    <div className={styles.recentActivity}>
-      {posts.length === 0 ? <p>No Posts</p> : (
-        <Post className={styles.recentActivityPost} />
-      )}
-      <Moment />
-    </div>
-    {console.log(posts)}
-    {console.log(moments)}
-  </div>
-);
+      <div className={`${styles.heading} ${styles.endHeading}`}>
+        <h3>Recent Activity</h3>
+      </div>
+      <div className={styles.recentActivity}>
+        {posts.length === 0 ? <p>No Posts</p> : renderLastPost()}
+        {moments.length === 0 ? <p>No Moments</p> : renderLastMoment()}
+      </div>
 
-// Possibilities:
-// User has NO POSTS / MOMENTS
-// User has only ONE POST / MOMENT
-// User has many of both, show the latest (length - 1)
+    </div>
+  );
+};
 
 Overview.propTypes = {
   posts: PropTypes.arrayOf(PropTypes.object).isRequired,
   moments: PropTypes.arrayOf(PropTypes.object).isRequired,
+  user: PropTypes.shape().isRequired,
 };
 
 const mapStateToProps = (state) => ({
   posts: state.users.user.posts,
   moments: state.users.user.moments,
+  user: state.auth.user,
 });
 
 export default connect(mapStateToProps)(Overview);
