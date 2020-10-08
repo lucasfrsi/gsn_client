2bd3bd09d5f11e7d9d7f692a326e96bf8444a010
Lucas F. Simoni <lucasfrsi@gmail.com>
2020-10-07 22:05:01 -0300

    Initial moment changes

--- src/components/Moment/index.jsx
+++ src/components/Moment/index.jsx
@@ -1,46 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-import image from '../../assets/images/default_avatar.png';
-import nani from '../../assets/images/reactions/nani.png';
-import love from '../../assets/images/reactions/love.png';
+import ProfileImage from '../UI/ProfileImage';
+
+import grr from '../../assets/images/reactions/grr.png';
 import haha from '../../assets/images/reactions/haha.png';
 import like from '../../assets/images/reactions/like.png';
+import love from '../../assets/images/reactions/love.png';
+import nani from '../../assets/images/reactions/nani.png';
+import sob from '../../assets/images/reactions/sob.png';
 
 import svg from '../../assets/svg/sprite.svg';
 
 import styles from './style.scss';
 
-const Moment = () => (
-  <div className={styles.moment}>
-    <div className={styles.momentHeading}>
-      <p className={styles.momentTitle}>This is a moment title</p>
-      <svg className={styles.momentPhotoButton}>
-        <use xlinkHref={`${svg}#icon-image`} />
-      </svg>
-    </div>
-    <div className={styles.momentText}>
-      One morning, when Gregor Samsa woke from troubled dreams,
-      he found himself transformed in his bed into a horrible vermin. He lay on his armour-like b.
-    </div>
-    <div className={styles.reactToMoment}>
-      <img src={like} alt="Like" />
-    </div>
-    <div className={styles.momentData}>
-      <div className={styles.momentReactionsBox}>
-        <div className={styles.momentReactions}>
-          <img src={nani} alt="Nani" className={styles.reactionImg} />
-          <img src={love} alt="Love" className={styles.reactionImg} />
-          <img src={haha} alt="Haha" className={styles.reactionImg} />
+const Moment = ({
+  user,
+  title,
+  text,
+  imageUrl,
+  reactions,
+  createdAt,
+  nickname,
+  avatar,
+}) => {
+  const [showImage, setShowImage] = useState(false);
+
+  return (
+    <div className={styles.moment}>
+      <div className={styles.momentHeading}>
+        <p className={styles.momentTitle}>{title}</p>
+        <svg className={styles.momentPhotoButton}>
+          <use xlinkHref={`${svg}#icon-image`} />
+        </svg>
+      </div>
+      <div className={styles.momentText}>{text}</div>
+      <div className={styles.reactToMoment}>
+        <img src={like} alt="Like" />
+      </div>
+      <div className={styles.momentData}>
+        <div className={styles.momentReactionsBox}>
+          <div className={styles.momentReactions}>
+            <img src={nani} alt="Nani" className={styles.reactionImg} />
+            <img src={love} alt="Love" className={styles.reactionImg} />
+            <img src={haha} alt="Haha" className={styles.reactionImg} />
+          </div>
+          <div className={styles.reactionCount}><span>67</span> Reactions</div>
         </div>
-        <div className={styles.reactionCount}><span>67</span> Reactions</div>
+        <div className={styles.momentUserBox}>
+          <p className={styles.momentUserName}>{nickname}</p>
+          <p className={styles.momentUserDate}>Aug 26<sup>th</sup>, 2020</p>
+        </div>
+        <ProfileImage profileImage={avatar} customStyle={styles.momentUserPhoto} />
       </div>
-      <div className={styles.momentUserBox}>
-        <p className={styles.momentUserName}>xCloudFFVII</p>
-        <p className={styles.momentUserDate}>Aug 26<sup>th</sup>, 2020</p>
-      </div>
-      <img src={image} alt="User 1" className={styles.momentUserPhoto} />
     </div>
-  </div>
-);
+  );
+};
+
+Moment.propTypes = {
+  user: PropTypes.string.isRequired,
+  nickname: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  reactions: PropTypes.arrayOf().isRequired,
+  createdAt: PropTypes.string.isRequired,
+};
 
 export default Moment;
---
+++ src/components/UI/ProfileImage/index.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import LoadingSpinner from '../LoadingSpinner';
+import defaultAvatar from '../../../assets/images/default_avatar.png';
+
+import styles from './style.scss';
+
+const ProfileImage = ({ profileImage, customStyle }) => {
+  const [loading, setLoading] = useState(true);
+
+  const handleOnLoad = () => {
+    setLoading(false);
+  };
+
+  const handleOnError = (event) => {
+    setLoading(false);
+    const img = event.target;
+    img.src = defaultAvatar;
+  };
+
+  return (
+    <>
+      {loading ? <LoadingSpinner size={customStyle} /> : null}
+      <img
+        src={profileImage || defaultAvatar}
+        alt="User Avatar"
+        className={`${styles.profileImage} ${customStyle}`}
+        onLoad={handleOnLoad}
+        onError={handleOnError}
+        style={{
+          display: loading ? 'none' : 'inline-block',
+        }}
+      />
+    </>
+  );
+};
+
+ProfileImage.propTypes = {
+  profileImage: PropTypes.string.isRequired,
+  customStyle: PropTypes.shape().isRequired,
+};
+
+export default ProfileImage;
