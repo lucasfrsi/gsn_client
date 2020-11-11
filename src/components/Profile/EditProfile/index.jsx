17854fddda8dc07df4cb01c0c0d799099963d03f
Lucas F. Simoni <lucasfrsi@gmail.com>
2020-11-11 11:06:51 -0300

    Continue implementing edit profile component

--- src/components/Profile/EditProfile/index.jsx
+++ src/components/Profile/EditProfile/index.jsx
@@ -37,27 +37,87 @@ const EditProfile = ({ close, updateProfile, currentProfile }) => {
 
   return (
     <div>
-      EDIT PROFILE
-      <br />
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="Real Name"
-          name="realName"
-          value={formData.realName}
-          onChange={onChange}
-        />
-        <input type="submit" />
+      <h1>Edit Profile</h1>
+      <p>Add some changes to your profile</p>
+      <form className={styles.form} onSubmit={onSubmit}>
+        <div className={styles.formGroup}>
+          <label htmlFor="realName">What is your name?</label>
+          <input
+            type="text"
+            placeholder="Real Name"
+            id="realName"
+            name="realName"
+            value={formData.realName}
+            onChange={onChange}
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="location">Where do you live?</label>
+          <input
+            type="text"
+            placeholder="Location"
+            id="location"
+            name="location"
+            value={formData.location}
+            onChange={onChange}
+          />
+          <small>City, state and country are suggested (e.g., <strong>Columbia, SC - USA</strong>)</small>
+        </div>
+        <div className={styles.formGroup}>
+          <p>What kind of player are you?</p>
+          <input
+            type="radio"
+            name="kind"
+            id="casual"
+            value="casual"
+            onChange={onChange}
+            checked={formData.kind === 'casual'}
+          />
+          <label htmlFor="casual">Casual</label>
+          <input
+            type="radio"
+            name="kind"
+            id="pro"
+            value="pro"
+            onChange={onChange}
+            checked={formData.kind === 'pro'}
+          />
+          <label htmlFor="pro">Professional</label>
+          <input
+            type="radio"
+            name="kind"
+            id="none"
+            value=""
+            onChange={onChange}
+            checked={formData.kind === ''}
+          />
+          <label htmlFor="none">None</label>
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="bio">Describe yourself</label>
+          <textarea
+            placeholder="Do you mind sharing a little about yourself? Max. 250 char"
+            id="bio"
+            name="bio"
+            value={formData.bio}
+            onChange={onChange}
+          />
+          <small>This might be what is separating you from finding your duo, right?</small>
+        </div>
+        <div className={styles.formGroup}>
+          {/* PLATFORMS: Icon button to show */}
+        </div>
+        <div className={styles.formGroup}>
+          {/* SOCIAL: Icon button to show */}
+        </div>
+        <button type="button" onClick={close}>Cancel</button>
+        <input type="submit" value="Submit" />
       </form>
       <br />
-      {formData.realName}
-      {formData.location}
-      {formData.kind}
-      {formData.platforms.nintendoswitch}
-      {formData.genres[0]}
       {formData.streamer}
       {formData.link}
-      {formData.bio}
+      {formData.genres[0]}
+      {formData.platforms.nintendoswitch}
       {formData.social.facebook}
     </div>
   );
