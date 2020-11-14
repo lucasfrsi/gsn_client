8dd69f5a40c334fb95a73f3fc6be605e4769da3c
Lucas F. Simoni <lucasfrsi@gmail.com>
2020-11-13 22:59:49 -0300

    Finish edit profile component

--- src/components/Profile/EditProfile/index.jsx
+++ src/components/Profile/EditProfile/index.jsx
@@ -9,6 +9,14 @@ import svg from '../../../assets/svg/sprite.svg';
 import styles from './style.scss';
 
 const GENRES = ['action', 'adventure', 'rpg', 'simulation', 'strategy', 'sports', 'mmo', 'card', 'fighting', 'platform'];
+const PLATFORMS = {
+  nintendoswitch: 'Nintendo Friend Code',
+  playstation: 'PSN ID',
+  xbox: 'Xbox Gamertag',
+  epicgames: 'Epic Games Username',
+  steam: 'Steam Friend Code',
+  discord: 'Discord Username & Tag',
+};
 
 const EditProfile = ({ close, updateProfile, currentProfile }) => {
   const [formData, setFormData] = useState({
@@ -87,15 +95,15 @@ const EditProfile = ({ close, updateProfile, currentProfile }) => {
   };
 
   return (
-    <div>
+    <div className={styles.editProfile}>
       <h1>Edit Profile</h1>
-      <p>Add some changes to your profile</p>
+      <h2>Add some changes to your profile</h2>
       <form className={styles.form} onSubmit={onSubmit}>
         <div className={styles.formGroup}>
-          <label htmlFor="realName">What is your name?</label>
+          <label htmlFor="realName" className={styles.description}>What is your real name?</label>
           <input
             type="text"
-            placeholder="Real Name"
+            placeholder="Park B. Johnson"
             id="realName"
             name="realName"
             value={formData.realName}
@@ -103,10 +111,10 @@ const EditProfile = ({ close, updateProfile, currentProfile }) => {
           />
         </div>
         <div className={styles.formGroup}>
-          <label htmlFor="location">Where do you live?</label>
+          <label htmlFor="location" className={styles.description}>Where do you live?</label>
           <input
             type="text"
-            placeholder="Location"
+            placeholder="Toronto, ON - Canada"
             id="location"
             name="location"
             value={formData.location}
@@ -115,7 +123,7 @@ const EditProfile = ({ close, updateProfile, currentProfile }) => {
           <small>City, state and country are suggested (e.g., <strong>Columbia, SC - USA</strong>)</small>
         </div>
         <div className={styles.formGroup}>
-          <p>What kind of player are you?</p>
+          <p className={styles.description}>What kind of player are you?</p>
           <input
             type="radio"
             name="kind"
@@ -124,7 +132,10 @@ const EditProfile = ({ close, updateProfile, currentProfile }) => {
             onChange={onChange}
             checked={formData.kind === 'casual'}
           />
-          <label htmlFor="casual">Casual</label>
+          <label htmlFor="casual" className={`${styles.radioLabel} ${formData.kind === 'casual' && styles.radioButtonChecked}`}>
+            <span className={styles.radioButton} />
+            Casual
+          </label>
           <input
             type="radio"
             name="kind"
@@ -133,7 +144,10 @@ const EditProfile = ({ close, updateProfile, currentProfile }) => {
             onChange={onChange}
             checked={formData.kind === 'pro'}
           />
-          <label htmlFor="pro">Professional</label>
+          <label htmlFor="pro" className={`${styles.radioLabel} ${formData.kind === 'pro' && styles.radioButtonChecked}`}>
+            <span className={styles.radioButton} />
+            Professional
+          </label>
           <input
             type="radio"
             name="kind"
@@ -142,10 +156,13 @@ const EditProfile = ({ close, updateProfile, currentProfile }) => {
             onChange={onChange}
             checked={formData.kind === ''}
           />
-          <label htmlFor="none">None</label>
+          <label htmlFor="none" className={`${styles.radioLabel} ${formData.kind === '' && styles.radioButtonChecked}`}>
+            <span className={styles.radioButton} />
+            None
+          </label>
         </div>
         <div className={styles.formGroup}>
-          <p>Are you a streamer?</p>
+          <p className={styles.description}>Are you a streamer?</p>
           <input
             type="radio"
             name="streamer"
@@ -154,7 +171,10 @@ const EditProfile = ({ close, updateProfile, currentProfile }) => {
             onChange={onBooleanChange}
             checked={formData.streamer === true}
           />
-          <label htmlFor="streamerTrue">Yes</label>
+          <label htmlFor="streamerTrue" className={`${styles.radioLabel} ${formData.streamer === true && styles.radioButtonChecked}`}>
+            <span className={styles.radioButton} />
+            Yes
+          </label>
           <input
             type="radio"
             name="streamer"
@@ -163,101 +183,130 @@ const EditProfile = ({ close, updateProfile, currentProfile }) => {
             onChange={onBooleanChange}
             checked={formData.streamer === false}
           />
-          <label htmlFor="streamerFalse">No</label>
+          <label htmlFor="streamerFalse" className={`${styles.radioLabel} ${formData.streamer === false && styles.radioButtonChecked}`}>
+            <span className={styles.radioButton} />
+            No
+          </label>
         </div>
         <div className={styles.formGroup}>
-          {formData.streamer ? <label htmlFor="link">What is your Twitch channel?</label> : <label htmlFor="link">What is your favorite Twitch streamer channel?</label>}
+          {formData.streamer ? <label htmlFor="link" className={styles.description}>What is your Twitch channel?</label> : <label htmlFor="link" className={styles.description}>What is your favorite Twitch streamer channel?</label>}
           <input
             type="text"
-            placeholder="Twitch Channel Name"
+            placeholder="ninja"
             id="link"
             name="link"
             value={formData.link}
             onChange={onChange}
+            className={styles.twitchPlaceholder}
           />
           <small>(1) Leave this field empty if you don&apos;t want to display the Twitch Player in your page</small>
           <small>(2) You must only write your channel name, not the entire address (e.g., https://www.twitch.tv/<strong>twitch</strong>)</small>
           <small>(3) Make sure you spell the channel name right, otherwise it will only display a black screen</small>
         </div>
         <div className={styles.formGroup}>
-          <label htmlFor="bio">Describe yourself</label>
-          <textarea
-            placeholder="Do you mind sharing a little about yourself? Max. 250 char"
-            id="bio"
-            name="bio"
-            value={formData.bio}
-            onChange={onChange}
-          />
-          <small>This might be what is separating you from finding your duo...</small>
+          <label htmlFor="bio" className={styles.description}>Describe yourself</label>
+          <div className={styles.textArea}>
+            <textarea
+              placeholder="Share a little about yourself =]"
+              id="bio"
+              name="bio"
+              value={formData.bio}
+              onChange={onChange}
+              autoComplete="new-password"
+            />{/* HANDLE 250 CHARACTERS LIMIT */}
+            <span className={`${styles.characterCount} ${formData.bio.length > 250 && styles.characterCountExceed}`}>{formData.bio.length}/250</span>
+          </div>
+          <small>This might be what is keeping you from finding your duo</small>
         </div>
         <div className={styles.formGroup}>
-          <p>Select your favorite genres</p>
-          {GENRES.map((genre) => (
-            <div key={genre}>
-              <input
-                type="checkbox"
-                name={genre}
-                id={genre}
-                checked={!formData.genres.includes(genre)}
-                onChange={onGenreChange}
-                style={{ display: 'none' }}
-              />
-              <label className={formData.genres.includes(genre) ? styles.selected : styles.notSelected} htmlFor={genre}>{genre}</label>
-            </div>
-          ))}
+          <p className={styles.description}>Select your favorite genres</p>
+          <div className={styles.genres}>
+            {GENRES.map((genre) => (
+              <div key={genre}>
+                <input
+                  type="checkbox"
+                  name={genre}
+                  id={genre}
+                  checked={!formData.genres.includes(genre)}
+                  onChange={onGenreChange}
+                  style={{ display: 'none' }}
+                />
+                <label className={`${styles.genre} ${formData.genres.includes(genre) ? styles.selected : styles.notSelected}`} htmlFor={genre}>{genre}</label>
+              </div>
+            ))}
+          </div>
         </div>
         <div className={styles.formGroup}>
-          <p>Platforms</p>
-          <svg onClick={() => setShowPlatforms(!showPlatforms)} className={`${styles.showIcon} ${showPlatforms && styles.invertShowIcon}`}>
-            <use xlinkHref={`${svg}#icon-chevron-down`} />
-          </svg>
+          <p className={styles.description}>
+            Platforms
+            <svg onClick={() => setShowPlatforms(!showPlatforms)} className={`${styles.showIcon} ${showPlatforms && styles.invertShowIcon}`}>
+              <use xlinkHref={`${svg}#icon-chevron-down`} />
+            </svg>
+          </p>
           {showPlatforms && (
-            Object.entries(formData.platforms).map(([key]) => (
-              <div key={key} className={styles.platform}>
-                <label htmlFor={key}>
-                  <svg className={styles.platformIcon}>
-                    <use xlinkHref={`${svg}#icon-${key}`} />
-                  </svg>
-                </label>
-                <input
-                  type="text"
-                  placeholder={key}
-                  id={key}
-                  name={key}
-                  value={formData.platforms[key]}
-                  onChange={onPlatformChange}
-                />
-              </div>
-            ))
+            <>
+              <p className={styles.smallDescription}>Share how people can find you in other platforms</p>
+              {Object.entries(formData.platforms).map(([key]) => (
+                <div key={key} className={styles.platform}>
+                  <label htmlFor={key}>
+                    <svg className={styles.platformIcon}>
+                      <use xlinkHref={`${svg}#icon-${key}`} />
+                    </svg>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder={PLATFORMS[key]}
+                    id={key}
+                    name={key}
+                    value={formData.platforms[key]}
+                    onChange={onPlatformChange}
+                  />
+                </div>
+              ))}
+            </>
           )}
         </div>
         <div className={styles.formGroup}>
-          <p>Social</p>
-          <svg onClick={() => setShowSocial(!showSocial)} className={`${styles.showIcon} ${showSocial && styles.invertShowIcon}`}>
-            <use xlinkHref={`${svg}#icon-chevron-down`} />
-          </svg>
+          <p className={styles.description}>
+            Social
+            <svg
+              onClick={(e) => {
+                e.target.scrollIntoView();
+                setShowSocial(!showSocial);
+              }}
+              className={`${styles.showIcon} ${showSocial && styles.invertShowIcon}`}
+            >
+              <use xlinkHref={`${svg}#icon-chevron-down`} />
+            </svg>
+          </p>
           {showSocial && (
-            Object.entries(formData.social).map(([key]) => (
-              <div key={key} className={styles.social}>
-                <label htmlFor={key}>
-                  <svg className={styles[key]}>
-                    <use xlinkHref={`${svg}#icon-${key}`} />
-                  </svg>
-                </label>
-                <input
-                  type="text"
-                  placeholder={key}
-                  id={key}
-                  name={key}
-                  value={formData.social[key]}
-                  onChange={onSocialChange}
-                />
-              </div>
-            ))
+            <>
+              <p className={styles.smallDescription}>Manage your social network links</p>
+              {Object.entries(formData.social).map(([key]) => (
+                <div key={key} className={styles.social}>
+                  <label htmlFor={key}>
+                    <svg className={styles[key]}>
+                      <use xlinkHref={`${svg}#icon-${key}`} />
+                    </svg>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder={key}
+                    id={key}
+                    name={key}
+                    value={formData.social[key]}
+                    onChange={onSocialChange}
+                  />
+                </div>
+              ))}
+              <small>Example: <strong>www.facebook.com/gamersx</strong></small>
+            </>
           )}
         </div>
-        <button type="button" onClick={close}>Cancel</button>
-        <input type="submit" value="Submit" />
+        <div className={styles.buttons}>
+          <button className={styles.cancel} type="button" onClick={close}>Cancel</button>
+          <input disabled={formData.bio.length > 250} className={styles.submit} type="submit" value="Submit" />
+        </div>
       </form>
     </div>
   );
