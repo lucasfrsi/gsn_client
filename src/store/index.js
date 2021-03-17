0d4e64f454723751ca889940c56508188c8d6907
Lucas F. Simoni <lucasfrsi@gmail.com>
2021-03-17 14:55:01 -0300

    Update webpack and libraries

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ import setAuthToken from '../util/setAuthToken';
 import rootReducer from './reducers';
 import rootSaga from './sagas';
 
-const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
+const composeEnhancers = (__webpack_devmode__ === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
 
 const sagaMiddleware = createSagaMiddleware();
 
